b972de847a02bcde585476fe066256af36125d24
CdP-Antoine <antoinepawlowski.ucatest@gmail.com>
2022-10-24 15:44:58 +0200

    Debug léger
    Debug slider-lieux
    Gestion bouton intro.php
    Gestion bouton intro.css

--- Index/intro.php
+++ Index/intro.php
@@ -4,4 +4,4 @@
 <p>Lorem Ipsum dolor sit amet blblblblb blblblblb blblblblb blblblblbl blblblblb blblblblblb
     blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb
     blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb</p>
-<a class="enSavoirPlus" href="description.php#cestQuoiLeGn"><h3>En savoir plus</h3></a>
+<a class="enSavoirPlus" href="description.php#cestQuoiLeGn">En savoir plus</a>
