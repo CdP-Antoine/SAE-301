110ef007e3417723c0c782ca2696117d0bc21844
R3D3D <marius.sct83@gmail.com>
2022-10-24 16:59:48 +0200

    header v1
    pas très beau mais passable à corriger et améliorer

--- Commun-Aux-Pages/header.php
+++ Commun-Aux-Pages/header.php
@@ -1,3 +1,32 @@
 <!-- Header -->
 
-<h2> Test Header </h2>
+<nav>
+    <ul>
+        <!-- Logo et Titre -->
+        <li><a href="index.php"><img src="../Illustrations/test.png" alt="Logo du Titre"></a></li>
+        <li><a href="index.php"><h1>La Guerre de l'Oiseau</h1></a></li>
+
+        <!-- Menu Description -->
+        <li>
+            <a href="description.php">Explications Histoire</a>
+            <ul>
+                <li><a href="description.php#cestQuoiLeGn">C'est quoi le G.N. ?</a></li>
+                <li><a href="description.php#clans">Les 4 clans</a></li>
+                <li><a href="description.php#lieux">Les 368 Lieux</a></li>
+            </ul>
+        </li>
+
+        <!-- Menu Déroulé -->
+        <li>
+            <a href="deroule.php">Activités Planning</a>
+            <ul>
+                <li><a href="description.php#cestQuoiLeGn"> Évènemements</a></li>
+                <li><a href="description.php#clans">Planning</a></li>
+                <li><a href="description.php#lieux">P'tits Guerriers</a></li>
+            </ul>
+        </li>
+
+        <!-- Menu Formulaire -->
+        <li><a href="formulaire.php"><img class="ticket" src="Illustrations/Commun-Aux-Pages/ticket.png" alt="Icone ticket"></a></li>
+    </ul>
+</nav>
