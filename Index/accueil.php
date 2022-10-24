abd7b1824e6bed302eed6c93c24beef885fd0a00
R3D3D <marius.sct83@gmail.com>
2022-10-24 11:56:16 +0200

    correction accueil + intro
    correction de base sur l'accueil et création de la structure intro avec un joli bouton (normalement)

--- Index/accueil.php
+++ Index/accueil.php
@@ -1,5 +1,3 @@
 <!-- Index > Accueil -->
 
-<section class="flexDebut">
-    <h2>Le Jeu de Rôle<br>Grandeur Nature<br>du Puy-en-Velay !</h2>
-</section>
+<h2>Le Jeu de Rôle Grandeur Nature du Puy-en-Velay !</h2>
--- Index/intro.php
+++ Index/intro.php
@@ -1,3 +1,7 @@
 <!-- Index > Intro -->
 
-<h2> Test Intro </h2>
+<h3>Introduction :</h3>
+<p>Lorem Ipsum dolor sit amet blblblblb blblblblb blblblblb blblblblbl blblblblb blblblblblb
+    blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb
+    blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb</p>
+<a class="enSavoirPlus" href="description.php#cestQuoiLeGn"><h3>En savoir plus</h3></a>
