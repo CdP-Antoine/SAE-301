00b1a62e7eb37953721f3a63dd81d4e11f7607cf
Enora <enora.faucheux@etu.uca.fr>
2022-10-24 10:54:08 +0200

    Update lieu-clan.php
    modification :
    ajout des paramètres, du constructeur et des méthodes

--- POO/lieu-clan.php
+++ POO/lieu-clan.php
@@ -2,17 +2,48 @@
 class LieuClan {
     // ----- Zone Paramètres 
 
-    // ----- FIN Zone Paramètres
+    public $type;
+    public $img;
+    public $desc;
+    public $titre;
+
+     // ----- FIN Zone Paramètres
 
 
 
     // ----- Constructeur
+
+    public function __construct($t, $i, $d, $tt){
+        $this->type = $t;
+        $this->img = $i;
+        $this->desc = $d;
+        $this->titre = $tt; 
+    }
+    
 
     // ----- FIN Constructeur
 
 
 
     // ----- Zone Méthodes 
+
+    public function afficher(){
+        echo $this->type;
+        echo "<br>";
+        echo $this->img;
+        echo "<br>";
+        echo $this->desc;
+        echo "<br>";
+        echo $this->titre;
+        echo "<br>";
+    }
+
+    public function modifier($t, $i, $d, $tt){
+        $this->type = $t;
+        $this->img = $i;
+        $this->desc = $d;
+        $this->titre = $t; 
+    }
 
     // ----- FIN Zone Méthodes
 }
