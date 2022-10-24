4bd68470c3d8e1739a9eddb8d80228d9c8893da5
Enora <enora.faucheux@etu.uca.fr>
2022-10-24 11:39:21 +0200

    footer.css
    ajout du footer dans le css pour pas se mélanger

--- Index/frise.php
+++ Index/frise.php
@@ -1,3 +1,18 @@
 <!-- Index > frise.php -->
 
-<h2> Test Frise </h2>
+<link href="frise.css" rel="stylesheet">
+
+<div>
+
+<figure>
+
+    <img src="Illustrations/Index/#" alt="frise">
+
+    <figcaption>
+
+    La frise chronologique pour comprendre l'histoire de la Guerre de l'Oiseau.
+
+    </figcaption>
+</figure>
+
+</div>
