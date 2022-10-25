a7e6876c1fefff451e85cbeae14b01365d8b99f8
Lau-lah <lola.courbon@orange.fr>
2022-10-25 14:25:28 +0200

    Mentions légales
    Ajout des mentions légales du site

--- Mentions/incl.php
+++
@@ -1,6 +0,0 @@
-<!-- Fichier d'inclusion dans mentions.php > main -->
-
-
-<section id="content">
-	<?php require_once("Mentions/content.php") ;?>
-</section>
--- mentions.php
+++ mentions.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Mon title Mentions </title>
+	<title> Mentions légales | La Guerre de l'Oiseau </title>
 
 	<meta name="description" content="">
 	<meta name="author" content="">
@@ -23,7 +23,66 @@
 </header>
 
 <main>
-	<?php require_once("Mentions/incl.php") ;?>
+	<div>
+		<h2>Mentions légales</h2>
+		<div>
+			<h3>Conditions d’utilisation</h3>
+			<p>
+			L’utilisation de ce site Internet vaut acceptation des conditions mentionnées ci-après. L'Association du Roi de l'Oiseau se réserve la possibilité d’engager des poursuites judiciaires en cas de violation de ces conditions d’utilisation, notamment en cas de non-respect et d’utilisation non autorisée des noms et emblèmes de l'Association du Roi de l'Oiseau ou en cas de violation des droits liés à l’information, au graphisme, aux textes, vidéos, documents audio, photos et images et plus généralement à l’ensemble du contenu du site.
+			</p>
+		</div>
+
+		<div>
+			<h3>Logo</h3>
+			<p>
+				Toute utilisation non autorisée du logo de La Guerre de l'Oiseau et de son appellation constitue un usage illégal sanctionné pénalement par le droit français.
+			</p>
+		</div>
+
+		<div>
+			<h3>Droits d'auteur</h3>
+			<p>
+				En application du Code français de la propriété intellectuelle et plus généralement des traités et accords internationaux comportant des dispositions relatives à la protection des droits d’auteurs, il est interdit pour un usage autre que privé ou pédagogique de reproduire, vendre, diffuser, modifier, publier intégralement ou partiellement, sous quelque forme que ce soit les données, la présentation ou l’organisation du site ainsi que les œuvres protégées par les droits d’auteur qui y figurent, sans autorisation écrite particulière et préalable, dont la demande doit être adressée au webmaster.
+			</p>
+		</div>
+
+		<div>
+			<h3>Liens </h3>
+			<p>
+			Les responsables de services en ligne qui créent des liens vers le présent site sont tenus d’en informer le webmaster. Ce site Internet peut comporter des liens vers d’autres sites qui ne sont pas sous contrôle permanent. Ces liens sont considérés comme utiles et pouvant enrichir l’information présente sur le site. L'Association du Roi de l'Oiseau n’endosse aucune responsabilité relative à ces sites, notamment en ce qui concerne l’exactitude ou la fiabilité des informations, des données et des opinions qui y figurent.</br>
+			L'Association du Roi de l'Oiseau autorise la mise en place d’un lien hypertexte pointant vers son contenu, sous réserve de ne pas utiliser la technique du lien profond (“deep linking”), c’est-à-dire que les pages du site ne doivent pas être imbriquées à l’intérieur des pages d’un autre site, mais accessible par l’ouverture d’une fenêtre. Mentionner la source qui pointera grâce à un lien hypertexte directement sur le contenu visé.
+			</p>
+		</div>
+
+		<div>
+			<h3>Portée des contenus</h3>
+			<p>
+				En dépit du soin apporté à la collecte des informations ainsi qu’à la réalisation du site au moyen des techniques les plus récentes, des erreurs, omissions, inexactitudes, coupures ou additions indépendantes de notre volonté peuvent demeurer ou s’insérer sur ce site. L’Association du Roi de l’oiseau ne peut, pour cette raison, être tenu à garantie, ni expressément, ni implicitement, pour l’ensemble du contenu du site ; l’utilisateur du site et des informations qu’il contient reconnaît qu’il en fait usage à ses seuls risques et périls et que l’Association du Roi de l’oiseau ne saurait en conséquence voir sa responsabilité engagée à raison de tout préjudice, direct ou indirect, de quelque nature que ce soit résultant pour tout ou partie de l’utilisation des informations du site.</br>
+
+				De même, l’Association du Roi de l’oiseau ne saurait voir sa responsabilité engagée en raison de la nature ou du contenu des sites référencés sur les pages qui suivent et notamment ceux pour lesquels un accès est permis par l’utilisation des liens hypertextes figurant aux pages de ce site. Conformément à la loi n°78-17 du 6 janvier 1978, vous disposez d’un droit d’accès et de rectification aux données vous concernant.
+			</p>
+		</div>
+
+		<div>
+			<h3>Protection des données personnelles</h3>
+			<p>
+				Aucune information personnelle n’est collectée à votre insu.
+				Aucune information personnelle n’est cédée à des tiers.
+			</p>
+		</div>
+
+		<div>
+			<h3>Association du Roi de l'Oiseau</h3>
+			<address>
+				Association du Roi de l’oiseau </br>
+				29, rue Raphaël </br>
+				43000 Le Puy en Velay </br>
+				04 71 09 38 41
+			</address>
+		</div>
+		
+
+	</div>
 </main>
 
 <footer>
