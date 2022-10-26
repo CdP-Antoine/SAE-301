805c78038c5165f50ab9f480077dbe5ac4c41537
enoradev <116085965+enoradev@users.noreply.github.com>
2022-10-26 11:28:16 +0200

    intro.css
    rajout de l'intro et réduction du bouton

--- Index/intro.php
+++ Index/intro.php
@@ -1,7 +1,15 @@
 <!-- Index > Intro -->
 
 <h3>Introduction :</h3>
-<p>Lorem Ipsum dolor sit amet blblblblb blblblblb blblblblb blblblblbl blblblblb blblblblblb
-    blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb
-    blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb blblblblb</p>
+<p>Lors de la dernière édition du Roi de l’Oiseau, le gagnant du tournois de tir qui a remporté les clés de 
+la ville et le titre de Roi, fut grandement contesté car il y aurait eu triche et erreur sur le gagnant. La 
+foule puis la ville se divisa et chaque Isle pris parti dans cette histoire qui pris des proportions 
+démesurées. Après une montée en tension de plusieurs mois entre les Isles, 4 clans se forment et la 
+guerre éclate en plein cœur de la ville pour le contrôle des clés.</p>
+<p>Venez participer ou assister à ce jeu de rôles grandeur nature (ou G.N.) qui fait suite au Roi de 
+l’Oiseau. Plusieurs centaines de participants vivant, dormant, et combattant comme à l’époque 
+médiévale, dans une grande simulation de guerre de plusieurs jours, au sein même du Puy-en-Velay, 
+où n’importe qui peut participer et contribuer à son échelle pour l’un des clans.</p>
+<p>Qui des poètes de Viguera, des barbares de Liostra, des stratèges de Laylen ou des artisants de 
+Morafen réussiront à s’emparer des clés de la ville, et à quel prix ? </p>
 <a class="enSavoirPlus" href="description.php#cestQuoiLeGn">En savoir plus</a>
