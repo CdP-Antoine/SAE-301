977c3486bdbf435c9755fd1e90852d2eed0c6775
CdP-Antoine <antoinepawlowski.ucatest@gmail.com>
2022-10-27 15:42:17 +0200

    Description dynamisé

--- Description/incl.php
+++ Description/incl.php
@@ -4,3 +4,15 @@
 <section id="content">
 	<?php require_once("Description/content.php") ;?>
 </section>
+
+<section id="clans">
+	<?php
+  	foreach($listeClan as $i) {
+      	$i->Afficher() ;
+  	}
+  	?>
+</section>
+
+<section id="slider-lieux">
+    <?php require_once("Index/slider-lieux.php") ;?>
+</section>
